8c14365ef9ddb210fbfb6d9898146f4f911c59bc
Browt22 <rsantiago4894@student.pps.net>
2026-03-04 11:38:01 -0800

    Final point stuff (for now)

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highscoreText;
+
+    int score = 0;
+    int highscore = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        scoreText.text = score.ToString() + " POINTS";
+        highscoreText.text = "HIGHSCORE: " + highscore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
