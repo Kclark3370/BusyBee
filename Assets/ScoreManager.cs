2c522e9568b2823ddd9df1d73ae917fb7402079f
Browt22 <rsantiago4894@student.pps.net>
2026-03-12 13:03:14 -0700

    Update ScoreManager.cs

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class ScoreManager : MonoBehaviour
 {
 
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI highscoreText;
+    public float repeatRate = 2.0f;
+    public float startDelay = 1.0f;
 
     int score = 0;
     int highscore = 0;
@@ -15,7 +19,14 @@ public class ScoreManager : MonoBehaviour
     {
         scoreText.text = score.ToString() + " POINTS";
         highscoreText.text = "HIGHSCORE: " + highscore.ToString();
+        InvokeRepeating(nameof(ScoreIncrease), startDelay, repeatRate);
     }
+
+    void ScoreIncrease()
+    {
+        score = score + 1;
+    }
+
 
     // Update is called once per frame
     void Update()
