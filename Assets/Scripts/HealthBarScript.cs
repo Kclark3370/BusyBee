54f7e68094fdb350c9bd484ab3778466348036a3
ZeroNix0 <christopher.jung26@gmail.com>
2026-03-12 13:44:04 -0700

    HealthBar finished

--- Assets/Scripts/HealthBarScript.cs
+++ Assets/Scripts/HealthBarScript.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 public class HealthBarScript : MonoBehaviour
 {
 
     public Slider healthBarSlider;
     public TextMeshProUGUI healthBarValueText;
+    public GameObject player;
+    public GameObject enemy1;
     public int maxHealth;
     public int currHealth;
 
@@ -20,6 +23,17 @@ public class HealthBarScript : MonoBehaviour
     {
         healthBarValueText.text = currHealth.ToString() + "/" + maxHealth.ToString();
         healthBarSlider.value = currHealth;
-        healthBarSlider.maxValue = maxHealth;
+
+        currHealth = Mathf.Clamp(currHealth, 0, maxHealth);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == enemy1)
+        {
+            Debug.Log("Damage");
+            currHealth = currHealth - 10;
+        }
+
     }
 }
