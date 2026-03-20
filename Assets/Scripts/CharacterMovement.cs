f347c599601778634aa4df5040cbf728fff25bae
Kclark3370 <kclark3370@student.pps.net>
2026-03-20 12:58:05 -0700

    added bee model, worked on movement code

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,10 +1,13 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
     private Vector2 input;
     public float speed = 0.5f;
-    public float turnSpeed = 1.0f;
+    public float turnSpeed = 2.5f;
+    public float turnAmp = 10f;
+    public GameObject model;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,14 +17,28 @@ public class Character : MonoBehaviour
     void Update()
     {
         input.x = Input.GetAxisRaw("Horizontal");
-
+        
         input.Normalize();
         transform.Translate(transform.up * speed * Time.deltaTime, Space.World);
+        model.transform.Rotate(Vector3.forward,turnSpeed*turnAmp* input.x);
     }
 
     void FixedUpdate()
     {
         transform.Rotate(Vector3.forward,turnSpeed * -input.x);
-        
+
+        RotateModel();
+    }
+
+    void RotateModel()
+    {
+        if (input.x > 0 & model.transform.rotation.z < 20)
+        {
+            model.transform.Rotate(Vector3.forward,turnSpeed*turnAmp* input.x);
+        }
+        if (input.x == 0)
+        {
+            model.transform.rotation = Quaternion.Euler(model.transform.rotation.x,0,0);
+        }
     }
 }
