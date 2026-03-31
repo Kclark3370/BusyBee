d7f8d291258beb7d9d6f87d145112a115c85b1ac
Kclark3370 <kclark3370@student.pps.net>
2026-03-31 12:43:34 -0700

    added bee texture

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,16 +1,21 @@
+using System;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
     private Vector2 input;
+    public Sprite[] spritesArray;
+    private SpriteRenderer spriteRenderer;
     public float speed = 0.5f;
     public float turnSpeed = 2.5f;
     public float turnAmp = 10f;
-    public GameObject model;
+
+    float _time;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
     }
 
     // Update is called once per frame
@@ -20,25 +25,10 @@ public class Character : MonoBehaviour
         
         input.Normalize();
         transform.Translate(transform.up * speed * Time.deltaTime, Space.World);
-        model.transform.Rotate(Vector3.forward,turnSpeed*turnAmp* input.x);
     }
 
     void FixedUpdate()
     {
         transform.Rotate(Vector3.forward,turnSpeed * -input.x);
-
-        RotateModel();
-    }
-
-    void RotateModel()
-    {
-        if (input.x > 0 & model.transform.rotation.z < 20)
-        {
-            model.transform.Rotate(Vector3.forward,turnSpeed*turnAmp* input.x);
-        }
-        if (input.x == 0)
-        {
-            model.transform.rotation = Quaternion.Euler(model.transform.rotation.x,0,0);
-        }
     }
 }
