c229156e5467996910a099591db41199dd42aee1
Kclark3370 <kclark3370@student.pps.net>
2026-03-31 13:55:02 -0700

    more bee movement :))))))))))

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,21 +1,17 @@
 using System;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
     private Vector2 input;
-    public Sprite[] spritesArray;
-    private SpriteRenderer spriteRenderer;
     public float speed = 0.5f;
     public float turnSpeed = 2.5f;
-    public float turnAmp = 10f;
-
-    float _time;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -25,6 +21,17 @@ public class Character : MonoBehaviour
         
         input.Normalize();
         transform.Translate(transform.up * speed * Time.deltaTime, Space.World);
+
+        if (Input.GetKey("space"))
+        {
+            speed = 20;
+            turnSpeed = 1.5f;
+        }
+        else
+        {
+            turnSpeed = 3.5f;
+            speed = 10;
+        }
     }
 
     void FixedUpdate()
