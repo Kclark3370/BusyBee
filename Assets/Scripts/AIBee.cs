eccdd63ed6fd1a4854546c6d32b7d2374ba9768d
Kclark3370 <kclark3370@student.pps.net>
2026-05-18 13:55:24 -0700

    added basic ai to combined scene
    yuhyuhyuh :))))))))

--- Assets/Scripts/AIBee.cs
+++ Assets/Scripts/AIBee.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.PlayerLoop;
 using UnityEngine.Rendering;
 
 public class AIBee : MonoBehaviour
@@ -12,32 +14,48 @@ public class AIBee : MonoBehaviour
     public List<GameObject> objectsInRange = new List<GameObject>();
     public string targetTag = "Flower";
     public GameObject target;
+    public GameObject closest;
+    public float speed = 5f;
+    NavMeshAgent agent;
 
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
     void Update()
     {
-        
         GetComponent<CircleCollider2D>().radius = scanRadius;
         CalculateDesire(transform.position);
+        agent.SetDestination(target.transform.position);
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag(targetTag)) {
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Flower"))
+        {
             objectsInRange.Add(other.gameObject);
         }
     }
 
-    void OnTriggerExit2D(Collider2D other) {
-        if (other.CompareTag(targetTag)) {
-            objectsInRange.Remove(other.gameObject);
-        }
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        objectsInRange.Remove(other.gameObject);
     }
 
     void CalculateDesire(Vector2 playerLocation)
     {
+        float closestDist = 100;
         foreach(GameObject flower in objectsInRange)
         {
-            float Flowerdistance = Vector2.Distance(playerLocation, flower.transform.position);
+            float objectDistance = Vector2.Distance(playerLocation,flower.transform.position);
+            if (objectDistance < closestDist)
+            {
+                closestDist = objectDistance;
+                closest = flower;
+            }
         }
-        
+        target = closest;
     }
 }
